a764ecff37c36d8bc459f56c2380ecaa060fd9e7
Juan Narvaez <juan.narvaez@cerner.com>
2019-12-31 00:06:35 -0600

    Pull all nationals in android build

---
+++ platforms/android/app/src/main/assets/www/app/sw.js
@@ -0,0 +1,154 @@
+var serviceWorkerOption = {
+  "assets": [
+    "/race-day-app4f0283c6ce28e888000e978e537a6a56.png",
+    "/race-day-app2273e3d8ad9264b7daa5bdbf8e6b47f8.png",
+    "/race-day-appa6137456ed160d7606981aa57c559898.png",
+    "/race-day-appindex.css",
+    "/race-day-appindex.js",
+    "/race-day-appindex.html"
+  ]
+};
+        
+        /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "app";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/site/service-worker.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/site/service-worker.js":
+/*!************************************!*\
+  !*** ./src/site/service-worker.js ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var RUNTIME = 'runtime';
+self.addEventListener('install', function (event) {
+  self.skipWaiting();
+}); // The activate handler takes care of cleaning up old caches.
+
+self.addEventListener('activate', function (event) {
+  console.log('activating');
+  var currentCaches = [RUNTIME];
+  event.waitUntil(caches.keys().then(function (cacheNames) {
+    return cacheNames.filter(function (cacheName) {
+      return !currentCaches.includes(cacheName);
+    });
+  }).then(function (cachesToDelete) {
+    return Promise.all(cachesToDelete.map(function (cacheToDelete) {
+      return caches.delete(cacheToDelete);
+    }));
+  }).then(function () {
+    console.log('claiming');
+    self.clients.claim();
+  }));
+});
+self.addEventListener('fetch', function (event) {
+  if (event.request.url.indexOf('tile.osm.org') >= 0) {
+    event.respondWith(caches.match(event.request).then(function (cachedResponse) {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return caches.open(RUNTIME).then(function (cache) {
+        return fetch(event.request).then(function (response) {
+          // Put a copy of the response in the runtime cache.
+          if (response.ok) {
+            return cache.put(event.request, response.clone()).then(function () {
+              return response;
+            });
+          } else {
+            return response;
+          }
+        });
+      });
+    }));
+  }
+});
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=sw.js.map
